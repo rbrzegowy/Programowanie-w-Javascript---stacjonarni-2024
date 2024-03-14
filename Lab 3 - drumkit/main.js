ad92bcff3ee20d2efd1c11bc80edbf9c4b109085
RB <r.brzegowy@gmail.com>
2024-03-14 10:53:15 +0100

    lab3

---
+++ Lab 3 - drumkit/main.js
@@ -0,0 +1,29 @@
+// const sound = document.querySelectorAll('sound')
+
+const sounds = {
+    'a': document.querySelector('#s1'),
+    's': document.querySelector('#s2'),
+    'd': document.querySelector('#s3'),
+}
+addEventListener('keypress',(ev)=>{
+    const key = ev.key
+    // switch(key) {
+    //     case 'a':
+    //         clap.currentTime = 0
+    //         clap.play()
+    //         break;
+    //     case 's':
+    //         kick.currentTime = 0
+    //         kick.play()
+    //         break;
+    //     case 'd':
+    //         hihat.currentTime = 0
+    //         hihat.play()
+    //         break;
+    // }
+    const sound = sounds[key]
+    console.dir(sound.dataset.key)
+    sound.currentTime= 0
+    sound.play()
+    
+})
